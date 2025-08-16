816b03b4ca1ff2ce0229c68f9c758b87528ec4b9
Dominik <93765669+143domi1@users.noreply.github.com>
2025-08-16 13:11:11 +0200

    Added the startup script

--- firmware/startup.c
+++ firmware/startup.c
@@ -1,1 +1,11 @@
+#include<stdio.h>
+#include<stdbool.h>
 
+int main(void) {
+    bool status = false;
+    float version = 0.1;
+    printf("Project Vigilante firmware\n");
+    printf("Status: %s\n", status ? "Online" : "Offline");
+    printf("Version %.1f\n", version);
+    return 0;
+}
