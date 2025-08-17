09812c236ec74567860f117c21c009a6f76b0654
Dominik <93765669+143domi1@users.noreply.github.com>
2025-08-17 20:16:06 +0200

    Reduced replication with unused calls , made status a global variable,  connected the start up sequence with the scanning sequence

--- firmware/startup.c
+++ firmware/startup.c
@@ -1,11 +1,26 @@
 #include<stdio.h>
 #include<stdbool.h>
+#include "scan.h"
+
+void scan_startup(void);
+
+bool status = false;
 
 int main(void) {
-    bool status = false;
     float version = 0.1;
     printf("Project Vigilante firmware\n");
     printf("Status: %s\n", status ? "Online" : "Offline");
     printf("Version %.1f\n", version);
+    scan_startup()
     return 0;
+}
+
+void scan_startup(void) {
+    if (status == true) {
+        while (status == true) {
+            scan_files();
+        }
+    } else {
+        printf("Not scanning\n");
+    }
 }
