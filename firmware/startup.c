f167db83b650ebd9e34e576eccb5f53e13bd1aaf
Dominik <93765669+143domi1@users.noreply.github.com>
2025-08-18 14:58:46 +0200

    Updated version to 0.2

--- firmware/startup.c
+++ firmware/startup.c
@@ -7,7 +7,7 @@ void scan_startup(void);
 bool status = false;
 
 int main(void) {
-    float version = 0.1;
+    float version = 0.2;
     printf("Project Vigilante firmware\n");
     printf("Status: %s\n", status ? "Online" : "Offline");
     printf("Version %.1f\n", version);
